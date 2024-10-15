1689e44499a776a60fa79e2d8712b2b6d02fbbf3
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2024-10-15 17:00:33 -0400

    Update HealthCheckController.java

--- src/main/java/com/example/webapp/controller/HealthCheckController.java
+++ src/main/java/com/example/webapp/controller/HealthCheckController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/healthz")
-public class HealthCheckController {
+public class HealthCheckController 
 
     private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
